349bd4ab7bb4a78d0d4343d2eb1dd9550cebbb3f
abkolbeko <sasha-kolbeko@mail.ru>
2023-03-11 20:10:24 +0500

    ToDO added

--- src/render.py
+++ src/render.py
@@ -3,3 +3,8 @@ from gui.containers.raw_observer import App
 if __name__ == "__main__":
     app = App()
     app.mainloop()
+
+
+# ToDo
+# solve problem with settings for frames initialization
+# https://stackoverflow.com/questions/7546050/switch-between-two-frames-in-tkinter
