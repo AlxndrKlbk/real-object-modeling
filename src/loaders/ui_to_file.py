5079fcfd55a058e130eb40801d3bd7964b4d1df6
abkolbeko <sasha-kolbeko@mail.ru>
2023-03-25 18:56:17 +0500

    some xml test and io usage

--- src/loaders/ui_to_file.py
+++ src/loaders/ui_to_file.py
@@ -4,6 +4,8 @@ __author__ = 'Kolbeko A.B.'
 
 # built-in
 from typing import Dict
+from io import StringIO
+import xml.etree.ElementTree as ET
 
 
 def save_to_xml(data: Dict) -> str:
@@ -19,3 +21,38 @@ def save_to_xml(data: Dict) -> str:
     # ToDo save to xml
     ...
     return ''
+
+
+if __name__ == '__main__':
+    root = ET.Element('a')
+    objects = ET.SubElement(root, 'objects_array')
+
+    for i in range(3):
+        item = ET.Element('item')
+        i_name = ET.Element('name')
+        i_name.text = f'separator_{i}'
+
+        item.append(i_name)
+        objects.append(item)
+
+    measurements = ET.SubElement(root, 'measuremens_array')
+    # ET.dump(root)
+    ET.indent(root)
+
+    # print(ET.tostring(root, encoding="unicode"))
+    # print(ET.tostring(root).decode())
+
+    file_path = '../../file.xml'
+
+    payload = ET.tostring(root).decode()
+    with open(file_path, 'w+') as file:
+        file.write(payload)
+
+    buffered = StringIO()
+    # buffered.write('<?xml version="1.0" encoding="UTF-8"?>')
+    buffered.write(payload)
+    buffered.seek(0)
+    read_file = ET.parse(buffered)
+
+    xml_tree = read_file.getroot()
+    print(ET.tostring(xml_tree).decode())
